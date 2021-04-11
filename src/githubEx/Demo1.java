8506e4ac3211b405310bb020a7a5689ad6f52137
Mohite <Mohite@DESKTOP-299SPQ7>
2021-04-11 15:27:46 +0530

    [dhansree] added sample1 class

---
+++ src/githubEx/Demo1.java
@@ -0,0 +1,11 @@
+package githubEx;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("How Are you?");
+		System.out.println("Good Afternoon");
+	}
+}
